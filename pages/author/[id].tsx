17ea41778c1479c97875f6aac55b2fd8944ba570
Ben Selby <benmatselby@gmail.com>
2021-05-20 18:47:54 +0100

    Provide the logic for the author routing
    Pulls authors from the files, and then caters for showing the author
    details.

--- pages/author/[id].tsx
+++ pages/author/[id].tsx
@@ -1,19 +1,38 @@
 import Layout from "../../components/layout";
 import Head from "next/head";
+import { getAllAuthorSlugs, getAuthorData } from "../../lib/authors";
+import { GetStaticPaths, GetStaticProps } from "next";
 
 /**
  * The author page component.
  */
-export default function Author({ authors }) {
+export default function Author({ author }) {
   return (
     <Layout>
       <Head>
         <title>Authors</title>
       </Head>
-      {authors}
+
       <article>
-        <h1>An author</h1>
+        <h1>{author.name}</h1>
       </article>
     </Layout>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllAuthorSlugs();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const author = await getAuthorData(params.id as string);
+  return {
+    props: {
+      author,
+    },
+  };
+};
