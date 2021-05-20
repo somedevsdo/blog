15f1da5e0442bdc58890d0af1f4937b0d47fd2b7
Andy Barnes <andrew.barnes102@googlemail.com>
2021-05-20 19:01:27 +0100

    Added very basic usage of prefers-color-scheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.scss";
 import "prismjs/themes/prism-tomorrow.css";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
