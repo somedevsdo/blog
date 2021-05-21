22bdfb414b1ed65a2a3b3e9c54b491c50cd92d62
Andy Barnes <andrew.barnes102@googlemail.com>
2021-05-21 10:50:44 +0100

    Made the document page more explicit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,15 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 
-function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+const Document = (): JSX.Element => (
+  <Html lang="en">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
 
 Document.getInitialProps = NextDocument.getInitialProps;
 Document.renderDocument = NextDocument.renderDocument;
