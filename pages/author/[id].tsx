a35af2442e14ee75a5b21dc167fca658c794c435
markgaze <mark.l.gaze@gmail.com>
2021-05-23 21:53:45 +0000

    Disallow unused imports

--- pages/author/[id].tsx
+++ pages/author/[id].tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Layout from "../../components/Layout/Layout";
 import { getAllAuthorSlugs, getAuthorData } from "../../lib/authors";
