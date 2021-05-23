5dc765a61b0b57d50399a75ab6ffbbf057893ba7
markgaze <mark.l.gaze@gmail.com>
2021-05-23 19:41:56 +0000

    Add Avatar tests

---
+++ components/Avatar/Avatar.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import Avatar from "./Avatar";
+
+describe("Layout", () => {
+  it("should render correctly", () => {
+    render(<Avatar src="/test.jpg" />);
+    expect(screen.getByAltText("Avatar")).toBeInTheDocument();
+  });
+
+  it("should be large by default", () => {
+    render(<Avatar src="/test.jpg" />);
+    expect(document.getElementsByClassName("avatar")[0].childNodes[0]).toHaveStyle("width: 152px");
+  });
+
+  it("should be the right size for small", () => {
+    render(<Avatar size="small" src="/test.jpg" />);
+    expect(document.getElementsByClassName("avatar")[0].childNodes[0]).toHaveStyle("width: 52px");
+  });
+
+  it("should be the right size for x-small", () => {
+    render(<Avatar size="x-small" src="/test.jpg" />);
+    expect(document.getElementsByClassName("avatar")[0].childNodes[0]).toHaveStyle("width: 40px");
+  });
+});
