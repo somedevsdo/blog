99e874bab30223a1146d465827815a315a719604
Andy Barnes <andrew.barnes102@googlemail.com>
2021-05-24 13:47:40 +0100

    Updated Cypress tests

--- cypress/integration/homepage.spec.ts
+++ cypress/integration/homepage.spec.ts
@@ -12,16 +12,21 @@ describe("Home page", () => {
   });
 
   it("should navigate to about", () => {
-    cy.get('[data-test-id="link-about"]').eq(0).click();
+    cy.get('[data-testid="link-about"]').eq(0).click();
     cy.url().should("include", "/about");
   });
 
   it("should navigate to authors", () => {
-    cy.get('[data-test-id="link-authors"]').eq(0).click();
+    cy.get('[data-testid="link-authors"]').eq(0).click();
     cy.url().should("include", "/authors");
   });
 
-  it("should navigate to a post", () => {
+  it("should show 3 posts", () => {
     cy.get('a[href*="post"]').should("have.length", 3);
+  });
+
+  it("should navigate to a post", () => {
+    cy.get('a[href*="post"]').eq(0).click();
+    cy.url().should("include", "/post");
   });
 });
