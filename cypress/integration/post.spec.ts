ad9e57e283617c7c43cb85208cfc655bd386b824
Andy Barnes <andrew.barnes102@googlemail.com>
2021-05-26 17:31:53 +0100

    Removed test for link we are going to remove anyway

--- cypress/integration/post.spec.ts
+++ cypress/integration/post.spec.ts
@@ -10,9 +10,4 @@ describe("Post", () => {
     cy.get("h1").should("contain", "Recent test page");
     cy.checkA11y(undefined, undefined, terminalLog);
   });
-
-  it("should navigate correctly", () => {
-    cy.get("a").eq(0).click();
-    cy.url().should("include", "/");
-  });
 });
