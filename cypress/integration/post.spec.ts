32e44420a4b5b7a7dca6dd1af5d66ab97f8c33fc
Andy Barnes <andrew.barnes102@googlemail.com>
2021-05-27 14:44:10 +0100

    Added an actual post to help define styling

--- cypress/integration/post.spec.ts
+++ cypress/integration/post.spec.ts
@@ -2,12 +2,12 @@ import { terminalLog } from "../support";
 
 describe("Post", () => {
   beforeEach(() => {
-    cy.visit("/post/recent");
+    cy.visit("/post/accessible-custom-focus-indicators");
     cy.injectAxe();
   });
 
   it("should render correctly", () => {
-    cy.get("h1").should("contain", "Recent test page");
+    cy.get("h1").should("contain", "Accessible custom focus indicators");
     cy.checkA11y(undefined, undefined, terminalLog);
   });
 });
