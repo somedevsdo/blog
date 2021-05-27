6f5c02bf51180217852d039db367a92299c85524
markgaze <mark.l.gaze@gmail.com>
2021-05-27 12:04:03 +0000

    Add an image component with a blur placeholder

--- components/Avatar/Avatar.spec.tsx
+++ components/Avatar/Avatar.spec.tsx
@@ -9,22 +9,22 @@ describe("Layout", () => {
 
   it("should be large by default", () => {
     render(<Avatar src="/test.jpg" />);
-    expect(document.getElementsByClassName("avatar")[0].childNodes[0]).toHaveStyle("width: 152px");
+    expect(document.getElementsByClassName("avatar")[0].childNodes[1]).toHaveStyle("width: 152px");
   });
 
   it("should be the right size for small", () => {
     render(<Avatar size="medium" src="/test.jpg" />);
-    expect(document.getElementsByClassName("avatar")[0].childNodes[0]).toHaveStyle("width: 77px");
+    expect(document.getElementsByClassName("avatar")[0].childNodes[1]).toHaveStyle("width: 77px");
   });
 
   it("should be the right size for small", () => {
     render(<Avatar size="small" src="/test.jpg" />);
-    expect(document.getElementsByClassName("avatar")[0].childNodes[0]).toHaveStyle("width: 52px");
+    expect(document.getElementsByClassName("avatar")[0].childNodes[1]).toHaveStyle("width: 52px");
   });
 
   it("should be the right size for x-small", () => {
     render(<Avatar size="x-small" src="/test.jpg" />);
-    expect(document.getElementsByClassName("avatar")[0].childNodes[0]).toHaveStyle("width: 40px");
+    expect(document.getElementsByClassName("avatar")[0].childNodes[1]).toHaveStyle("width: 40px");
   });
 
   it("should have a border", () => {
