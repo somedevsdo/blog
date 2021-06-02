70a5e89c565694936fcd5ee4da6f3ae86df02ce2
markgaze <mark.l.gaze@gmail.com>
2021-06-02 13:50:31 +0000

    Use actual post data

--- lib/posts.ts
+++ lib/posts.ts
@@ -50,28 +50,39 @@ export interface IPost {
  *
  * @returns all posts sorted by date
  */
-export const getSortedPostsData = (): { id: string; date: string; title: string }[] => {
+export const getSortedPostsData = async (): Promise<IPost[]> => {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, "");
+  const allPostsData = await Promise.all(
+    fileNames.map(async (fileName) => {
+      // Remove ".md" from file name to get id
+      const id = fileName.replace(/\.md$/, "");
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+      // Read markdown file as string
+      const fullPath = path.join(postsDirectory, fileName);
+      const fileContents = fs.readFileSync(fullPath, "utf8");
 
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+      // Use gray-matter to parse the post metadata section
+      const matterResult = matter(fileContents);
 
-    // Combine the data with the id
-    return {
-      id,
-      ...(matterResult.data as { date: string; title: string }),
-    };
-  });
+      const authorProfile = await getAuthorData(matterResult.data.author);
+
+      const { category, date, featuredImage, title } = matterResult.data;
+
+      // Combine the data with the id
+      return {
+        authorProfile,
+        category,
+        date,
+        featuredImage,
+        id,
+        title,
+      } as IPost;
+    })
+  );
+
   // Sort posts by date
-  return allPostsData.sort((a, b) => {
+  return allPostsData.sort((a: IPost, b: IPost): number => {
     if (a.date < b.date) {
       return 1;
     }
