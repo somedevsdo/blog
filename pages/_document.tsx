8ff438788e2a7bb8b07b9a370fd807ab88072c80
Andy Barnes <andrew.barnes102@googlemail.com>
2021-06-30 06:59:45 +0100

    Add Poppins and Noto Sans to the document so they can be used

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,25 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 
-const Document = (): JSX.Element => (
-  <Html lang="en">
-    <Head />
-    <body>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-);
+const Document = (): JSX.Element => {
+  const fontLink =
+    "https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&family=" +
+    "Poppins:wght@700;900&display=swap";
+
+  return (
+    <Html lang="en">
+      <Head>
+        <link href="https://fonts.googleapis.com" rel="preconnect" />
+        <link crossOrigin="" href="https://fonts.gstatic.com" rel="preconnect" />
+        <link href={fontLink} rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
 Document.getInitialProps = NextDocument.getInitialProps;
 Document.renderDocument = NextDocument.renderDocument;
