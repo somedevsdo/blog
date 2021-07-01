62375f09bc859447c75bc63af00913f8e2d91335
Andy Barnes <andrew.barnes102@googlemail.com>
2021-07-01 09:52:43 +0100

    Updated social link with some icons and classname
    We will need to move the icons somewhere more centrally at some point.

--- components/SocialLink/SocialLink.spec.tsx
+++ components/SocialLink/SocialLink.spec.tsx
@@ -4,21 +4,21 @@ import SocialLink from "./SocialLink";
 describe("SocialLink", () => {
   it("should render correctly", () => {
     render(<SocialLink link="https://bobbins.com" />);
-    expect(screen.getByTitle("Web link")).toBeInTheDocument();
+    expect(screen.getByTitle("Visit web page")).toBeInTheDocument();
   });
 
   it("should render a GitHub link correctly", () => {
     render(<SocialLink link="https://github.com/benmatselby" />);
-    expect(screen.getByTitle("GitHub link")).toBeInTheDocument();
+    expect(screen.getByTitle("Visit GitHub page")).toBeInTheDocument();
   });
 
   it("should render a Twitter link correctly", () => {
     render(<SocialLink link="https://twitter.com/benmatselby" />);
-    expect(screen.getByTitle("Twitter link")).toBeInTheDocument();
+    expect(screen.getByTitle("Visit Twitter page")).toBeInTheDocument();
   });
 
   it("should render a Dev.tp link correctly", () => {
     render(<SocialLink link="https://dev.to/benmatselby" />);
-    expect(screen.getByTitle("Dev.to link")).toBeInTheDocument();
+    expect(screen.getByTitle("Visit Dev.to page")).toBeInTheDocument();
   });
 });
