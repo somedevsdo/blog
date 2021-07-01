e237424d6c896c6bdbb5a9cefae550b00217d8cf
Andy Barnes <andrew.barnes102@googlemail.com>
2021-07-01 14:15:46 +0100

    Updated card to match designs
    - Breaks homepage layout, but that can be picked up in its own issue
    - There is no hover / focus style yet
    - Author component did not fit new design so well, so can be revisited later
    - Hard coded roles in since they were not in the IAuthor interface

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import getDateFormatted from "../../lib/date";
 import { IPost } from "../../lib/posts";
-import Author from "../Author/Author";
+import Avatar from "../Avatar/Avatar";
 import ImageWithPlaceholder from "../ImageWithPlaceholder/ImageWithPlaceholder";
 import styles from "./Card.module.scss";
 
@@ -13,31 +13,37 @@ const Card = (props: ICardProps): JSX.Element => {
   const { post } = props;
   const { authorProfile, category, date, featuredImage, id, subtitle, title } = post;
   return (
-    <div className={styles.card}>
-      <div className={styles.header}>
-        <ImageWithPlaceholder
-          alt={title}
-          layout="fill"
-          objectFit="cover"
-          src={`/posts/featured/${featuredImage}`}
-        />
-      </div>
-      <div className={styles.details}>
-        <Link href={`/post/${id}`}>
-          <a className={styles.link}>
-            <span className={styles.postDetails}>
-              <p className={styles.category}>{category}</p> {" - "}
-              <p className={styles.date}>{getDateFormatted(date)}</p>
-            </span>
-            <p className={styles.title}>{title}</p>
-            <p className={styles.subtitle}>{subtitle}</p>
-          </a>
-        </Link>
-        <div className={styles.author}>
-          <Author author={authorProfile} avatarBorder={false} avatarSize="x-small" />
-        </div>
-      </div>
-    </div>
+    <>
+      <Link href={`/post/${id}`}>
+        <a className={styles.link} title={title}>
+          <div className={styles.container}>
+            <div className={styles.image}>
+              <ImageWithPlaceholder
+                alt={title}
+                layout="fill"
+                objectFit="cover"
+                src={`/posts/featured/${featuredImage}`}
+              />
+            </div>
+            <div className={styles.content}>
+              <span className={styles.meta}>
+                <span className={styles.category}>{category}</span> {" - "}
+                <span>{getDateFormatted(date)}</span>
+              </span>
+              <h2 className={styles.title}>{title}</h2>
+              <p className={styles.subtitle}>{subtitle}</p>
+              <div className={styles.author}>
+                <Avatar border={false} size="x-small" src={authorProfile.profile} />
+                <div>
+                  <div className={styles.name}>{authorProfile.name}</div>
+                  <div className={styles.role}>UI Developer</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </Link>
+    </>
   );
 };
 
