18d1c126f45fe54eb421ef138bf46709e469a198
Andy Barnes <andrew.barnes102@googlemail.com>
2022-05-14 13:42:23 +0100

    Mocked ImageWithPlaceholder in Avatar

--- components/Avatar/Avatar.spec.tsx
+++ components/Avatar/Avatar.spec.tsx
@@ -1,29 +1,32 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Avatar from "./Avatar";
+import ImageWithPlaceholder from "../ImageWithPlaceholder/ImageWithPlaceholder";
+import Image from "next/image";
+
+jest.mock("../ImageWithPlaceholder/ImageWithPlaceholder");
+
+(ImageWithPlaceholder as jest.Mock).mockReturnValue(
+  <Image alt="avatar" layout="fill" src="/test.jpg" />
+);
 
 describe("Avatar", () => {
-  it("should render correctly", () => {
-    render(<Avatar src="/test.jpg" />);
-    expect(screen.getByAltText("Avatar")).toBeInTheDocument();
-  });
-
   it("should be large by default", () => {
     render(<Avatar src="/test.jpg" />);
-    expect(document.getElementsByClassName("avatar")[0].childNodes[0]).toHaveStyle("width: 120px");
+    expect(document.getElementsByClassName("avatar")[0]).toHaveStyle("width: 120px");
   });
 
   it("should be the right size for small", () => {
     render(<Avatar size="medium" src="/test.jpg" />);
-    expect(document.getElementsByClassName("avatar")[0].childNodes[0]).toHaveStyle("width: 77px");
+    expect(document.getElementsByClassName("avatar")[0]).toHaveStyle("width: 77px");
   });
 
   it("should be the right size for small", () => {
     render(<Avatar size="small" src="/test.jpg" />);
-    expect(document.getElementsByClassName("avatar")[0].childNodes[0]).toHaveStyle("width: 52px");
+    expect(document.getElementsByClassName("avatar")[0]).toHaveStyle("width: 52px");
   });
 
   it("should be the right size for x-small", () => {
     render(<Avatar size="x-small" src="/test.jpg" />);
-    expect(document.getElementsByClassName("avatar")[0].childNodes[0]).toHaveStyle("width: 40px");
+    expect(document.getElementsByClassName("avatar")[0]).toHaveStyle("width: 40px");
   });
 });
