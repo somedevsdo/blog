783a46f08ea1a695a01616f9c4ba5c1df7d19bcd
markgaze <mark.l.gaze@gmail.com>
2022-09-06 17:53:41 +0100

    Use UnoCSS for icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
-import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
+import unocss from "@unocss/astro";
+import presetIcons from "@unocss/preset-icons";
+import presetUno from "@unocss/preset-uno";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), mdx(), svelte()]
+  integrations: [
+    image(),
+    mdx(),
+    svelte(),
+    unocss({
+      presets: [presetIcons(), presetUno()],
+    }),
+  ],
+  vite: {
+    build: {
+      cssCodeSplit: true,
+    },
+  },
 });
