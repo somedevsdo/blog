d4a60ba61e68bfd7047cf79554a9dcaccdc25d76
davidn0rman <rockbigdave@gmail.com>
2026-02-28 11:32:19 +0000

    Refactor Cypress configuration to use named import for plugins

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "cypress";
+import plugins from "./cypress/plugins/index.js";
 
 export default defineConfig({
   video: false,
@@ -7,7 +8,8 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
+      plugins(on, config);
+      return config;
     },
     baseUrl: "http://localhost:4321",
   },
